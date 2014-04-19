2fa51cbbd1d8c934514a3c402172ac360e87e52c
Rune Fevang <rune.fevang@gmail.com>
2014-04-19 13:23:59 -0700

    Set up logging.

--- visualizer/main.js
+++ visualizer/main.js
@@ -1,13 +1,19 @@
 goog.provide('monoid.main');
 
 goog.require('goog.dom');
+goog.require('goog.debug.Console');
 goog.require('monoid.LoadRaceButton');
+
 
 goog.scope(function() {
 
 
 
 monoid.main = function() {
+  // Set up logging.
+  goog.debug.Console.autoInstall();
+  goog.debug.Console.instance.setCapturing(true);
+
   var loadRaceButton = new monoid.LoadRaceButton();
   loadRaceButton.render(goog.dom.getDocument().body);
 };
